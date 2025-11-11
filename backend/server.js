288158f8a1ca9c7258627a8172b8d33e2f335736
Misukisti <misukisti@gmail.com>
2025-11-12 00:27:22 +0200

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,54 @@
+import path from "path";
+import express from "express"; 
+import dotenv from "dotenv";
+import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
+import {v2 as cloudinary} from "cloudinary";
+
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+
+
+
+
+dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+const app = express(); 
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+app.use(express.json({limit:"5mb"})); //express middleware joka kertoo että kaikki json tallennetaan req.bodyyn, limitoi kuvan koko jotta Dos hyökkäyksiltä vältytään
+
+
+app.use(express.urlencoded({extended: true})); // to parse form datea
+
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "frontend/dist");
+  app.use(express.static(frontendPath));
+
+  // Catch-all route for React
+  app.get(/^(?!\/api).*/, (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+    connectMongoDB();
+}); 
